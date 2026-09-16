bccdabc6dbce82b689b0e87752b4637ce3a2aa11
That Khiem Ton <29520244+th4tkh13m@users.noreply.github.com>
2026-09-15 21:24:26 -0400

    Add: longest-substring-without-duplicates - submission-7

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-7.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Idea: Using sliding window:
+        # Left: At the character when the dup is found
+        # Right: Continue checking
+        if len(s) == 0:
+            return 0
+        max_len = 0
+        left = 0
+
+        for right in range(len(s)):
+            if s[right] in s[left:right]:
+                left = left + s[left: right].index(s[right] ) + 1
+            current_len = right - left + 1
+            max_len = max(current_len, max_len)
+        return max_len
